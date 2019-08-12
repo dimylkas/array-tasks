4e564c4d1a2f12939f6f5ee3a3b7a941cd119d9c
oleksandr.navka <oleksandr.navka@globallogic.com>
2019-08-12 10:51:31 +0300

    Added solution for tasks

--- src/main/java/com/globallogic/arrays/Tasks.java
+++ src/main/java/com/globallogic/arrays/Tasks.java
@@ -8,7 +8,12 @@ public class Tasks {
         The sequence of integers is given. Increase by 2 each its non-negative element.
      */
     public int[] increaseBy2(int[] arr) {
-        return null;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                arr[i] = arr[i] + 2;
+            }
+        }
+        return arr;
     }
 
     /*
@@ -17,6 +22,12 @@ public class Tasks {
         In the case of absence the specified element print "Not Found"
     */
     public double findFirstNumber(double[] arr) {
+        for (double value : arr) {
+            if (value < 2.5) {
+                return value;
+            }
+        }
+        System.out.println("Not found");
         return 0;
     }
 
@@ -25,6 +36,17 @@ public class Tasks {
         The sequence of real numbers is given. Find the arithmetic mean of positive numbers.
      */
     public double arithmeticalMean(double[] arr) {
+        double positiveNumbersSum = 0;
+        int positiveNumbersCounter = 0;
+        for (double element : arr) {
+            if (element > 0) {
+                positiveNumbersSum += element;
+                positiveNumbersCounter++;
+            }
+        }
+        if (positiveNumbersCounter != 0) {
+            return positiveNumbersSum / positiveNumbersCounter;
+        }
         return 0;
     }
 
@@ -32,7 +54,13 @@ public class Tasks {
         The sequence of integers is given. Find max int value in sequence
      */
     public int findMax(int[] arr) {
-        return 0;
+        int max = arr[0];
+        for (int element : arr) {
+            if (max < element) {
+                max = element;
+            }
+        }
+        return max;
     }
 
     /*
@@ -40,7 +68,13 @@ public class Tasks {
         The sequence of real numbers is given. Lets find their absolute values. Find the maximum value among these absolute values.
      */
     public double maxOfAbsoluteValues(double[] arr) {
-        return 0;
+        double max = arr[0];
+        for (double element : arr) {
+            if (Math.abs(element) > Math.abs(max)) {
+                max = element;
+            }
+        }
+        return max;
     }
 
     /*
@@ -48,7 +82,13 @@ public class Tasks {
         The sequence of n real numbers is given. Find the sum and the number of negative elements in it.
      */
     public double negativeElementsSum(double[] arr) {
-        return 0;
+        double sum = 0;
+        for (double element : arr) {
+            if (element < 0) {
+                sum += element;
+            }
+        }
+        return sum;
     }
 
     /*
@@ -56,6 +96,11 @@ public class Tasks {
     Array of int is given. Return the given array in the reverse order
      */
     public int[] reverseArray(int[] arr) {
-        return null;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = tmp;
+        }
+        return arr;
     }
 }
